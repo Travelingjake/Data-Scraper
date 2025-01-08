bcdfeb989d0972cbfd6662039aa8679f005ca8d1
David MacFarquhar <macfarfar@gmail.com>
2025-01-08 18:32:08 -0500

    depivot updated

--- functions.py
+++ functions.py
@@ -6,4 +6,6 @@ def depivot_data(data):
     # depends on parties being listed in all caps and metadata being listed with lowercase
     metadata_columns = [c for c in data.columns if not re.search(r'\b[A-Z]+\b', c)]
     data = pd.melt(data, id_vars=metadata_columns, var_name='Party')
+    if '%' in data.loc['value', 0]:
+        data['value'].apply(lambda x: x[:-1])
     return data
